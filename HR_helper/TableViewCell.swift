7db10fac3f8e0477f4b66d662106197aa9a697c2
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-07-30 14:07:32 +0300

    added extension for using name of costume cell's classes bu identifier.

---
+++ HR_helper/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  HR_helper
+//
+//  Created by Oleksii Kolomiiets on 7/30/18.
+//  Copyright © 2018 Oleksii Kolomiiets. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableViewCell {
+    class var reuseIdentifier: String {
+        return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+
+    static var nib: UINib {
+        return UINib(nibName: reuseIdentifier, bundle: nil)
+    }
+}
---
+++ HR_helper/Viewes/EmployeeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeeTableViewCell.swift
+//  HR_helper
+//
+//  Created by Oleksii Kolomiiets on 7/30/18.
+//  Copyright © 2018 Oleksii Kolomiiets. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
