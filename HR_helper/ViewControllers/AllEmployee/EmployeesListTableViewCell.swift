bc1c538c5055da0806cbe3cd145d9948d0e8dd30
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-07-30 15:32:02 +0300

    deleted template code.

---
+++ HR_helper/ViewControllers/AllEmployee/EmployeesListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeesListTableViewCell.swift
+//  HR_helper
+//
+//  Created by Oleksii Kolomiiets on 7/30/18.
+//  Copyright © 2018 Oleksii Kolomiiets. All rights reserved.
+//
+
+import UIKit
+
+class EmployeesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HR_helper/ViewControllers/AllEmployee/EmployeesListTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EmployeesListTableViewController.swift
+//  HR_helper
+//
+//  Created by Oleksii Kolomiiets on 7/30/18.
+//  Copyright © 2018 Oleksii Kolomiiets. All rights reserved.
+//
+
+import UIKit
+
+class EmployeesListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: EmployeeTableViewCell.reuseIdentifier, for: indexPath)
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let embededVC = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: EmployeeDetailsViewController.reuseIdentifier) as? EmployeeDetailsViewController else { return }
+        self.navigationController?.pushViewController(embededVC, animated: true)
+    }
+
+}
