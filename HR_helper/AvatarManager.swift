7920c302d61f545289913f3d099b538291261e9e
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-07-31 16:44:02 +0300

    added avatar manager.

---
+++ HR_helper/AvatarManager.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarManager.swift
+//  HR_helper
+//
+//  Created by Oleksii Kolomiiets on 7/31/18.
+//  Copyright © 2018 Oleksii Kolomiiets. All rights reserved.
+//
+
+import UIKit
+
+class AvatarManager {
+    var image: UIImage
+    
+    init(with image: UIImage) {
+        self.image = image
+    }
+}
