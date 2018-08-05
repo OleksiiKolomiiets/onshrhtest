14a3af7c95bb54963d9004370c82f72df5575c6c
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-08-06 00:10:35 +0300

    separated models.

--- HR_helper/Models/EmployeeEntity.swift
+++ HR_helper/Models/EmployeeEntity.swift
@@ -36,44 +36,7 @@ extension EmployeeEntity: Hashable {
     }
 }
 
-var fakeData: [EmployeeEntity] = [
-    EmployeeEntity(name: "Leo Messi", position: "Forward", birthDay: Date(timeIntervalSince1970: TimeInterval(153725975)), avatar: AvatarManager(with: #imageLiteral(resourceName: "Image")) ),
-    EmployeeEntity(name: "Ivan Rakitic", position: "Midfilder", birthDay: Date(timeIntervalSince1970: TimeInterval(253725111)), avatar: nil),
-    EmployeeEntity(name: "Ter Shtegen", position: "Goalkeeper", birthDay: Date(timeIntervalSince1970: TimeInterval(353722375)), avatar: AvatarManager(with: #imageLiteral(resourceName: "iconHarry")))
-]
 
-class DataSource {
-    
-    private var employees: Set<EmployeeEntity> = [
-        EmployeeEntity(name: "Leo Messi", position: "Forward", birthDay: Date(timeIntervalSince1970: TimeInterval(153725975)), avatar: AvatarManager(with: #imageLiteral(resourceName: "Image")) ),
-        EmployeeEntity(name: "Ivan Rakitic", position: "Midfilder", birthDay: Date(timeIntervalSince1970: TimeInterval(253725111)), avatar: nil),
-        EmployeeEntity(name: "Ter Shtegen", position: "Goalkeeper", birthDay: Date(timeIntervalSince1970: TimeInterval(353722375)), avatar: AvatarManager(with: #imageLiteral(resourceName: "iconHarry")))
-    ]
-    
-    var count: Int {
-        return employees.count
-    }
-    
-    func getAll() -> Set<EmployeeEntity> {
-        return employees
-    }
-    
-    func add(_ employee: EmployeeEntity) {
-        var trmporaryEmployee = employee
-        if employees.contains(employee) {
-            trmporaryEmployee.setUniqName()
-        }
-        employees.insert(trmporaryEmployee)
-    }
-    
-    func delete(_ employee: EmployeeEntity) {
-        employees.remove(employee)
-    }
-    
-    subscript(index: Int) -> EmployeeEntity {
-        let i =  employees.index(employees.startIndex, offsetBy: index)
-        return employees[i]
-    }
-}
+
 
 
